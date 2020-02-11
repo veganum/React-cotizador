1d41f5792849f450871019dc9e0396343cacfc77
José <veganum@hotmail.com>
2020-02-11 23:00:56 +0100

    Finalizando los Styled Components del Formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -23,6 +23,24 @@ const InputRadio = styled.input`
     margin: 0 1rem;
 `;
 
+const Boton = styled.button`
+    background-color: #00838F;
+    font-size: 16px;
+    width: 100%;
+    padding: 1rem;
+    color: #fff;
+    text-transform: uppercase;
+    font-weight: bold;
+    border: none;
+    transition: background-color .3s ease;
+    margin-top: 2rem;
+
+    &:hover {
+        background-color: #26C6DA;
+        cursor:pointer;
+    }
+`;
+
 const Formulario = () => {
     return (
         <form>
@@ -67,7 +85,7 @@ const Formulario = () => {
                 />Completo
             </Campo>
 
-            <button type="button">Cotizar</button>
+            <Boton type="button">Cotizar</Boton>
 
         </form>
     );
